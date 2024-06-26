e7dab79bc2f90217150a4558b78d85c836a8ef95
CZY <chenzhenyusg@gmail.com>
2024-06-26 09:59:24 +0800

    1nd

---
+++ tamper.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-26
+// @description  try to take over the world!
+// @author       You
+// @match        http://127.0.0.1:31510/?code=b3ee9f6c0ce30c292070&state=bea3fceb2ab94388a8fd48ee9222fd44
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log("hello world");
+    confirm("我帅不帅")
+    // Your code here...
+})();
