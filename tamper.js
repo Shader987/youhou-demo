c576fda9a5ab09a628f76d5cd9fe99ecdcd09af5
CZY <chenzhenyusg@gmail.com>
2024-06-26 14:21:55 +0800

    ver 3

--- tamper.js
+++ tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hello world
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.3
 // @description  new ss
 // @author       You
 // @match        *://*
